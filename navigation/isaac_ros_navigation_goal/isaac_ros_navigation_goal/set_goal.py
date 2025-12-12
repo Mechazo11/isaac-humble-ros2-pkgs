b2c1d3bf6c671ba9d47c3047bc2288dd98bb7926
Mechazo11 <azmyin12@gmail.com>
2025-12-12 11:24:14 -0600

    updated to Isaac Sim 5.1.0 release

--- navigation/isaac_ros_navigation_goal/isaac_ros_navigation_goal/set_goal.py
+++ navigation/isaac_ros_navigation_goal/isaac_ros_navigation_goal/set_goal.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
@@ -147,7 +162,7 @@ class SetNavigationGoal(Node):
         """
         # Nav2 is sending empty message for success as well as for failure.
         result = future.result().result
-        self.get_logger().info("Result: {0}".format(result.result))
+        self.get_logger().info("Result: {0}".format(result))
 
         if self.curr_iteration_count < self.MAX_ITERATION_COUNT:
             self.curr_iteration_count += 1
